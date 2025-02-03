3635aab85214d73fba7d92566e9d3435bb951ead
Graham Bing <gdbing@users.noreply.github.com>
2025-02-03 14:58:17 -0800

    Refactor Node insertion logic
    - Split insert into insertIntoBranch and insertIntoLeaf
    - Add helper method splitNode

--- Sources/TendrilTree/Node+Insertion.swift
+++ Sources/TendrilTree/Node+Insertion.swift
@@ -6,81 +6,76 @@
 import Foundation
 
 extension Node {
-    func insert(content: String, at offset: Int) {
-        cacheString = nil
-        if offset == 0
-        {
-            if let left {
-                weight += content.utf16Length
-                left.insert(content: content, at: 0)
-            } else {
-                if content.last != "\n" {
-                    self.content = content + self.content!
-                    self.weight += content.utf16Length
-                } else {
-                    self.left = Node()
-                    left!.content = content
-                    left!.weight = content.utf16Length
-                    
-                    self.right = Node()
-                    right!.content = self.content
-                    right!.weight = self.content?.utf16Length ?? 0
-                    
-                    self.content = nil
-                    self.weight = left!.weight
-                }
-            }
-        }
-        else if offset < weight
-        {
-            if let left {
-                left.insert(content: content, at: offset)
-                self.weight += content.utf16Length
-            } else {
-                let offsetIndex = self.content!.charIndex(utf16Index: offset)!
-                if content.last != "\n" {
-                    self.content!.insert(contentsOf: content, at: offsetIndex)
-                    self.weight += content.utf16Length
-                } else {
-                    self.left = Node()
-                    let leftSubString = self.content!.prefix(upTo: offsetIndex)
-                    left!.content = String(leftSubString) + content
-                    left!.weight = left!.content!.utf16Length
-                    
-                    self.right = Node()
-                    let rightSubString = self.content!.suffix(from: offsetIndex)
-                    right!.content = String(rightSubString)
-                    right!.weight = right!.content!.utf16Length
-                    
-                    self.content = nil
-                    self.weight = left!.weight
-                }
-            }
-        }
-        else // offset >= weight
-        {
-            if let right {
-                right.insert(content: content, at: offset - weight)
-            } else {
-                guard offset == weight else { fatalError() }
-                
-                if self.content?.last != "\n" {
-                    self.content! += content
-                    weight += content.utf16Length
-                } else {
-                    self.left = Node()
-                    left!.content = self.content
-                    left!.weight = self.content?.utf16Length ?? 0
-                    
-                    self.right = Node()
-                    right!.content = content
-                    right!.weight = content.utf16Length
-                    
-                    self.content = nil
-                }
-            }
+    private var isLeaf: Bool {
+        return content != nil
+    }
+    
+    internal func insert(content: String, at offset: Int) {
+        if isLeaf {
+            insertIntoLeaf(content, at: offset)
+        } else {
+            insertIntoBranch(content, at: offset)
         }
         
+    }
+    
+    @inlinable
+    internal func insertIntoBranch(_ insertion: String, at offset: Int) {
+        cacheString = nil
+
+        if offset < weight {
+            if let left {
+                left.insert(content: insertion, at: offset)
+                self.weight += insertion.utf16Length
+            } else {
+                fatalError("insertIntoBranch: missing left child")
+            }
+        } else {
+            if let right {
+                right.insert(content: insertion, at: offset - weight)
+            } else {
+                fatalError("insertIntoBranch: missing right child")
+            }
+        }
         self.balance()
+    }
+    
+    @inlinable
+    internal func insertIntoLeaf(_ insertion: String, at offset: Int) {
+        guard let content = self.content, let offsetIndex = content.charIndex(utf16Index: offset) else {
+            fatalError("insertIntoLeaf: missing text or offset out of bounds")
+        }
+        
+        let prefix = content.prefix(upTo: offsetIndex)        
+        if prefix.hasSuffix("\n") {
+            // appending under the last paragraph
+            splitNode(leftContent: String(prefix), rightContent: insertion)
+            return
+        }
+        
+        if insertion.hasSuffix("\n") {
+            splitNode(leftContent: prefix + insertion, rightContent: String(content.suffix(from: offsetIndex)))
+            return
+        }
+        
+        self.content = content.prefix(upTo: offsetIndex) + insertion + String(content.suffix(from: offsetIndex))
+        self.weight += insertion.utf16Length
+    }
+    
+    
+    @inlinable
+    internal func splitNode(leftContent: String, rightContent: String) {
+        let left = Node()
+        left.content = leftContent
+        left.weight = leftContent.utf16Length
+        
+        let right = Node()
+        right.content = rightContent
+        right.weight = rightContent.utf16Length
+        
+        self.left = left
+        self.right = right
+        self.content = nil
+        self.weight = leftContent.utf16Length
     }
 }
