d24d707c97930b279584dadba18126b549ebc15f
Graham Bing <gdbing@users.noreply.github.com>
2025-04-28 15:29:31 -0700

    Clean up unused String Utility functions

--- Sources/TendrilTree/String Utilities.swift
+++ Sources/TendrilTree/String Utilities.swift
@@ -2,59 +2,10 @@
 //  String Utilities.swift
 //  TendrilTree
 //
-//  Provides String extensions for UTF-16 indexing, range conversion,
-//  and line/paragraph handling needed by TendrilTree.
-//
-//  Key Features:
-//
-//  - UTF-16 <-> String.Index conversion and mapping.
-//  - `utf16Length` convenience accessor.
-//  - `splitIntoLines`: Enumerates substrings by line endings, preserving '\n',
-//    suitable for preparing content for `Node.parse`.
-//
 
 import Foundation
 
 extension String {
-    func range(from utf16Range: NSRange) -> Range<String.Index>? {
-        guard
-            let from16 = utf16.index(utf16.startIndex, offsetBy: utf16Range.location, limitedBy: utf16.endIndex),
-            let to16 = utf16.index(from16, offsetBy: utf16Range.length, limitedBy: utf16.endIndex),
-            let from = String.Index(from16, within: self),
-            let to = String.Index(to16, within: self)
-            else { return nil }
-
-        return from ..< to
-    }
-
-    func utf16Range(from range: Range<String.Index>) -> NSRange? {
-        guard
-            let from = range.lowerBound.samePosition(in: utf16),
-            let to = range.upperBound.samePosition(in: utf16)
-        else { return nil }
-
-        return NSRange(location: utf16.distance(from: utf16.startIndex, to: from),
-                       length: utf16.distance(from: from, to: to))
-    }
-
-    func utf16Range(charRange: NSRange) -> NSRange? {
-        guard charRange.upperBound <= count else { return nil }
-
-        let lowerBound = index(startIndex, offsetBy: charRange.lowerBound)
-        let upperBound = index(startIndex, offsetBy: charRange.upperBound)
-        let indexRange: Range<String.Index> = lowerBound ..< upperBound
-        return utf16Range(from: indexRange)
-    }
-
-    func charRange(utf16Range: NSRange) -> NSRange? {
-        guard let indexRange = Range(utf16Range, in: self) else { return nil }
-
-        let location = indexRange.lowerBound.utf16Offset(in: self)
-        let length   = indexRange.upperBound.utf16Offset(in: self) - location
-
-        return NSRange(location: location, length: length)
-    }
-
     public func charIndex(utf16Index: Int) -> String.Index? {
         guard let idx = utf16.index(utf16.startIndex, offsetBy: utf16Index, limitedBy: utf16.endIndex)
         else { return nil }
@@ -62,15 +13,6 @@ extension String {
         return String.Index(idx, within: self)
     }
 
-    public func utf16Index(charIndex: String.Index) -> Int? {
-        guard let from = charIndex.samePosition(in: utf16)
-        else { return nil }
-
-        return utf16.distance(from: utf16.startIndex, to: from)
-    }
-}
-
-extension String {
     @inline(__always)
     var utf16Length: Int {
         self.utf16.count
@@ -90,40 +32,4 @@ extension StringProtocol {
         }
         return lines
     }
-}
-
-@available(macOS 13.3, iOS 16.4, watchOS 9.4, tvOS 16.4, *)
-extension StringProtocol {
-  @inline(__always)
-  var _indexOfLastCharacter: Index {
-    guard !isEmpty else { return endIndex }
-    return index(before: endIndex)
-  }
-
-  @inline(__always)
-  func _index(at offset: Int) -> Index {
-    self.index(self.startIndex, offsetBy: offset)
-  }
-
-  @inline(__always)
-  func _utf8Index(at offset: Int) -> Index {
-    self.utf8.index(startIndex, offsetBy: offset)
-  }
-
-  @inline(__always)
-  func _utf8ClampedIndex(at offset: Int) -> Index {
-    self.utf8.index(startIndex, offsetBy: offset, limitedBy: endIndex) ?? endIndex
-  }
-
-  @inline(__always)
-  func _utf8Offset(of index: Index) -> Int {
-    self.utf8.distance(from: startIndex, to: index)
-  }
-
-  @inline(__always)
-  var _lastCharacter: (index: Index, utf8Length: Int) {
-    let i = _indexOfLastCharacter
-    let length = utf8.distance(from: i, to: endIndex)
-    return (i, length)
-  }
 }
